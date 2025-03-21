a7225d79c514069e10745e54807f07774386c9d1
May <expert_ss@naver.com>
2025-03-21 17:05:06 +0900

    20250321_업데이트

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -33,12 +33,12 @@ function Portfolio() {
       <h2>PORTFOLIO</h2>
       <div className="frame-container" ref={containerRef}>
         <div className="frame">
-          <img src="/images/pf_image01.png" alt="삼성 포트폴리오" />
+          <img src="/my-portfolio/images/pf_image01.png" alt="삼성 포트폴리오" />
           <p>기본적인 퍼블리싱이 가능한 것을 보여주기 위해 작업한 개인 작업물</p>
           <Link to="https://soul-git-s9.github.io/song2023verPf/samsung/samsung.html">작업물 바로가기 &gt;</Link>
         </div>
         <div className="frame">
-          <img src="/images/pf_image02.png" alt="부트스트랩 포트폴리오" />
+          <img src="/my-portfolio/images/pf_image02.png" alt="부트스트랩 포트폴리오" />
           <p>부트스트랩으로 간단한 쇼핑몰을 구현해 본 개인 작업물</p>
           <Link to="https://soul-git-s9.github.io/song2023verPf/shoppingmall/shoppingmall.html">작업물 바로가기 &gt;</Link>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import './index.css'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/my-portfolio">
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/Portfolio" element={<Portfolio />} />
